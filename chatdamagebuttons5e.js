01d628aed0c0aa64bb6b5f10600fe15cd0bf2f2b
Hooking <shaun.newsome@gmail.com>
2019-04-14 10:03:22 +1200

    Improved styling and exposed to players

--- chatdamagebuttons5e.js
+++ chatdamagebuttons5e.js
@@ -6,41 +6,42 @@ class ChatDamageButtons5e extends Application {
     init () {
 
         Hooks.on('renderChatMessage', (message, data, html) => {
-            // do nothing if this is not a roll chat-message or if the roll is a d20 (as that is likely to be an attack roll)
             if ( !message.isRoll || message.roll.parts[0].faces == 20 ) return;
+            let btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
 
-            const fullDamageButton = $('<button class="dice-total-fullDamage-btn" style="max-width: 10%;"><i class="fas fa-user-minus" title="Click to apply full damage to selected token(s)."></i></button>');
-            const halfDamageButton = $('<button class="dice-total-halfDamage-btn" style="max-width: 10%;"><i class="fas fa-user-shield" title="Click to apply half damage to selected token(s)."></i></button>');
-            const doubleDamageButton = $('<button class="dice-total-doubleDamage-btn" style="max-width: 10%;"><i class="fas fa-user-injured" title="Click to apply double damage to selected token(s)."></i></button>');
-            const fullHealingButton = $('<button class="dice-total-fullHealing-btn" style="max-width: 10%;"><i class="fas fa-user-plus" title="Click to apply full healing to selected token(s)."></i></button>');
+            const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply full damage to selected token(s)."></i></button>`);
+            const halfDamageButton = $(`<button class="dice-total-halfDamage-btn" style="${btnStyling}"><i class="fas fa-user-shield" title="Click to apply half damage to selected token(s)."></i></button>`);
+            const doubleDamageButton = $(`<button class="dice-total-doubleDamage-btn" style="${btnStyling}"><i class="fas fa-user-injured" title="Click to apply double damage to selected token(s)."></i></button>`);
+            const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-user-plus" title="Click to apply full healing to selected token(s)."></i></button>`);
 
-            if (game.user.isGM) {
-                html.find('.dice-total').append(fullDamageButton);
-                html.find('.dice-total').append(halfDamageButton);
-                html.find('.dice-total').append(doubleDamageButton);
-                html.find('.dice-total').append(fullHealingButton);
+            const btnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
+            btnContainer.append(fullDamageButton);
+            btnContainer.append(halfDamageButton);
+            btnContainer.append(doubleDamageButton);
+            btnContainer.append(fullHealingButton);
 
-                // Handle button clicks
-                fullDamageButton.click(ev => {
-                    ev.stopPropagation();
-                    Actor5e.applyDamage(html, 1);
-                });
-                
-                halfDamageButton.click(ev => {
-                    ev.stopPropagation();
-                    Actor5e.applyDamage(html, 0.5);
-                });
+            html.find('.dice-total').append(btnContainer);
 
-                doubleDamageButton.click(ev => {
-                    ev.stopPropagation();
-                    Actor5e.applyDamage(html, 2);
-                });
+            // Handle button clicks
+            fullDamageButton.click(ev => {
+                ev.stopPropagation();
+                Actor5e.applyDamage(html, 1);
+            });
+              
+            halfDamageButton.click(ev => {
+                ev.stopPropagation();
+                Actor5e.applyDamage(html, 0.5);
+            });
 
-                fullHealingButton.click(ev => {
-                    ev.stopPropagation();
-                    Actor5e.applyDamage(html, -1);
-                });
-            }
+            doubleDamageButton.click(ev => {
+                ev.stopPropagation();
+                Actor5e.applyDamage(html, 2);
+            });
+
+            fullHealingButton.click(ev => {
+                ev.stopPropagation();
+                Actor5e.applyDamage(html, -1);
+            });
 
         })
     }
